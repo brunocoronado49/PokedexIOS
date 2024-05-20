d1a52069e44aaae3f740d05aa9e49a528f98c517
brunocoronado49 <francorangelcoronado@gmail.com>
2024-05-20 17:03:18 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Bruno  on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
